3d82fbe31ba9c694df5f5791fd1a051904d1afc0
Divya Prakash <91613905+div-yash@users.noreply.github.com>
2024-08-04 13:00:40 +0530

    Create 283. Move Zeroes.java

---
+++ 283. Move Zeroes.java
@@ -0,0 +1,16 @@
+https://leetcode.com/problems/move-zeroes/
+
+
+class Solution {
+    public void moveZeroes(int[] nums) {
+        for(int i=0;i<nums.length-1;i++){
+            for(int j=0;j<nums.length-1;j++){
+            if(nums[j]==0){
+                int temp=nums[j];
+                nums[j]=nums[j+1];
+                nums[j+1]=temp;
+            }
+        }
+        }
+    }
+}
