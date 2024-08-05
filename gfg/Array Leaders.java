b93d169698a5ed89cc8b203461abd0491d45fd70
Divya Prakash <91613905+div-yash@users.noreply.github.com>
2024-08-05 10:54:55 +0530

    Create Array Leaders.java

---
+++ gfg/Array Leaders.java
@@ -0,0 +1,70 @@
+https://www.geeksforgeeks.org/problems/leaders-in-an-array-1587115620/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=leaders-in-an-array
+
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+
+class Array {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter ot = new PrintWriter(System.out);
+        int t = Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+
+        while (t-- > 0) {
+
+            // input size of array
+            int n = Integer.parseInt(br.readLine().trim());
+            int arr[] = new int[n];
+            String inputLine[] = br.readLine().trim().split(" ");
+
+            // inserting elements in the array
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            Solution obj = new Solution();
+
+            StringBuffer str = new StringBuffer();
+            ArrayList<Integer> res = new ArrayList<Integer>();
+
+            // calling leaders() function
+            res = obj.leaders(n, arr);
+
+            for (int i = 0; i < res.size(); i++) {
+                ot.print(res.get(i) + " ");
+            }
+
+            ot.println();
+        }
+        ot.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    // Function to find the leaders in the array.
+    static ArrayList<Integer> leaders(int n, int arr[]) {
+        // Your code here
+        int max=arr[n-1];
+        
+        ArrayList<Integer> lst=new ArrayList<>();
+        lst.add(max);
+        for(int i=n-2;i>=0;i--){
+            if(arr[i]>max){
+                max=arr[i];
+                lst.add(max);
+            }
+        }
+        ArrayList<Integer> ans=new ArrayList<Integer>();
+        for(int  i=lst.size()-1;i>=0;i--){
+            ans.add(lst.get(i));
+        }
+        
+        
+        return ans;
+    }
+}
